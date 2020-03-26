d88662dd658bff20e1ca4607677aac9980b9986e
13jorge <54521719+13jorge@users.noreply.github.com>
2020-03-25 18:42:37 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,24 @@
 let array = [];
 let backgroundColor = 200;
-let noiseOffset= 0.0;
-let strokeWidth=5;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(20,200,100);
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
-drawGrid();
-// strokeWeight(5);
-// noFill();
+function setup() {
+  createCanvas(windowWidth/2, windowHeight/2);
+  background(232, 24, 10);
+
+  drawGrid();
+  // strokeWeight(5);
+  // noFill();
 
 }
 
 function draw() {
-  background(20,200,100,10);
+  // background(232,24,10,10);
   strokeWeight(strokeWidth);
 
-noiseOffset+= 0.09;
-strokeWidth= noise(noiseOffset) * 70;
+  noiseOffset += 0.09;
+  strokeWidth = noise(noiseOffset) * 70;
 
   if (mouseIsPressed) {
     stroke(map(mouseX, 59, 200, 150, 255, true))
@@ -39,38 +40,39 @@ function keyTyped() {
   } else if (key === 'c') {
     clear();
     //display image
-//     background(255);
-// beginShape();
-//     for (let i = 0; i < array.length; i++) {
-//       // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
-//       curveVertex(array[i][0], array[i][1]);
-// endShape();
-//     }
+    //     background(255);
+    // beginShape();
+    //     for (let i = 0; i < array.length; i++) {
+    //       // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
+    //       curveVertex(array[i][0], array[i][1]);
+    // endShape();
+    //     }
   }
   return false;
 }
 
-// function mousePressed(){
-// array=[];
-// backgroundColor=255;
-//
-// }
+function mousePressed() {
+  array = [];
+  backgroundColor = 255;
 
-function drawGrid(){
-  numCells=20;
-  fillColor=255;
+}
+
+function drawGrid() {
+  numCells = 10;
+  let startColor = color(20, 200, 160);
+  let endColor = color(200, 200, 0);
+  let fillColor;
   noStroke();
   strokeWeight(0);
-for (let i=0;i<= width; i +=width/numCells){
-  for(let j = 0; j<=height; j+= height/numCells){
-    if(fillColor===255){
-      fillColor=200
-    }else{
-      fillColor=255
+  for (let i = 0; i <= width; i += width / numCells) {
+    for (let j = 0; j <= height; j += height / numCells) {
+      fillColor = lerpColor(startColor, endColor, j / height);
+      fill(fillColor);
+          rect(i, j, width / numCells, height / numCells);
     }
-    fill(fillColor)
-  rect(i,j,width/numCells, height/numCells);
-}
-}
+
+
+  }
+
 strokeWeight(5)
 }
